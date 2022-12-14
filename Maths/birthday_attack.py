121e9574204106342034bcb9100aea993ca26e0c
Songyue Wang <me@songyue.wang>
2022-12-14 15:53:40 +0800

    Updated maths PDF

--- Maths/birthday_attack.py
+++ Maths/birthday_attack.py
@@ -3,9 +3,10 @@ import math
 def calculateProb(d, n):
   exponent = (-n * (n - 1)) / (2 * d)
   return 1 - math.e ** exponent;
+  
+# calculate the probability that at least 2 people share the same birthday, given there is a total of 23 people
+print(calculateProb(365, 23)) 
 
 #calculate the probability that there is a collision if the hash has 8 hex characters, and there are 1000 hashing operations
 print(calculateProb(16**8, 1000)) 
 
-# calculate the probability that at least 2 people share the same birthday, given there is a total of 23 people
-print(calculateProb(365, 23)) 
